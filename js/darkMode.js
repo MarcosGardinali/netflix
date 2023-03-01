9fd571888d4427628beefb0924ee364cb29b1251
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-03-01 09:01:52 -0300

    Criando a const do botão dark mode

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,4 +1,6 @@
-document.querySelector("#btn").addEventListener("click", () => {
+const botaoDarkMode = document.querySelector("#btn");
+
+botaoDarkMode.addEventListener("click", () => {
 	document.body.classList.toggle("dark-mode")
 });
 console.log("Dark Mode Sincronizado!!")
